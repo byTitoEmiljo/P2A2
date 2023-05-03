608d197c7e9930edcfc8899fd835eec5e627752a
byTitoEmiljo <119460094+byTitoEmiljo@users.noreply.github.com>
2023-05-03 11:42:24 -0600

    Create main.php

---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Página de ejemplo</title>
+	<script>
+		function mostrarMensaje() {
+			alert("Hola mundo");
+		}
+	</script>
+</head>
+<body>
+	<h1>Bienvenido a mi página web</h1>
+	<button onclick="mostrarMensaje()">Haz clic aquí</button>
+</body>
+</html>
